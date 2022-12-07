cc7d91bda89b6a8964a8de98e5702587bb19aabc
Lamiaaahmed89 <lylyahmed336@gmail.com>
2022-12-07 02:11:23 +0200

    splash screen

--- graduation_project/lib/constants/colors.dart
+++ graduation_project/lib/constants/colors.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+Color  Main_color=const Color.fromRGBO(40,95,250,1);
+Color Second_color=const Color.fromRGBO(37,38,50,1);
+Color Text2_color=const Color.fromRGBO(174,178,187,1);
+Color white_color=const Color.fromRGBO(255,255,255,1);
 
-Color Main_color=const Color.fromRGBO(40,95,250,255);
-Color Second_color=const Color.fromRGBO(37,38,50,255);
-Color Text2_color=const Color.fromRGBO(174,178,187,255);
-Color white_color=const Color.fromRGBO(255,255,255,255);
+Color Success_color=const Color.fromRGBO(40,205,151,1);
+Color Failed_color=const Color.fromRGBO(250,17,73,1);
 
-Color Success_color=const Color.fromRGBO(40,205,151,255);
-Color Failed_color=const Color.fromRGBO(250,17,73,255);
+
 
 
