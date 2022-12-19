a3e7b05caaa003c27f78cafd7e39526bcb38089c
Lamiaaahmed89 <lylyahmed336@gmail.com>
2022-12-20 00:50:55 +0200

    "

--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/view/screens/onboardin.dart';
 import 'package:graduation_project/view/screens/splachscreen.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
 
 void main() {
@@ -13,9 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // builder:(context, widget) =>ResponsiveWrapper.builder(ClampingScrollWrapper.builder(context, widget!) ,
-      // breakpoints:[const ResponsiveBreakpoint.resize(300,name:MOBILE),
-      // const ResponsiveBreakpoint.autoScale(300)],),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily:'Poppins'),
       home:SplashScreen(),
