1ce22543a9300d1843d1943ebd1b78de8d7f13de
Lamiaaahmed89 <lylyahmed336@gmail.com>
2022-12-21 00:27:22 +0200

    assets

--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:graduation_project/view/Appointment_pages/upcomming.dart';
 import 'package:graduation_project/view/screens/onboardin.dart';
 import 'package:graduation_project/view/screens/splachscreen.dart';
 
@@ -11,10 +13,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily:'Poppins'),
-      home:SplashScreen(),
+      home:UpComming(),
     );
   }
 }
