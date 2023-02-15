4c8f483cf9810b15408a042bc293902d5f11090d
lamiaa <lylyahmed336@gmail.com>
2023-02-15 02:26:30 +0200

    .

--- graduation_project/lib/constants/colors.dart
+++ graduation_project/lib/constants/colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-Color  Main_color=const Color.fromRGBO(40,95,250,1);
+Color Main_color=const Color.fromRGBO(40,95,250,1);
 Color Second_color=const Color.fromRGBO(37,38,50,1);
 Color Text2_color=const Color.fromRGBO(174,178,187,1);
 Color white_color=const Color.fromRGBO(255,255,255,1);
--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:graduation_project/view/screens/onboardin.dart';
 import 'package:graduation_project/view/screens/splachscreen.dart';
 
+import 'view/screens/test.dart';
+
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarBrightness:Brightness.dark,statusBarColor:Colors.white));
   runApp(const MyApp());
 }
 
@@ -11,9 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily:'Poppins'),
-      home:SplashScreen(),
+      theme: ThemeData(
+          fontFamily: 'Poppins',
+      ),
+      home: OnboardingScreen(),
     );
   }
 }
-
