8db2566f90dab44c8a0083aab321224e8c1470dc
lamiaa <lylyahmed336@gmail.com>
2023-03-29 01:10:58 +0200

    ..

--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:graduation_project/view/Appointment_pages/appointment_no_dates.d
 import 'package:graduation_project/view/Appointment_pages/choose_appointment.dart';
 import 'package:graduation_project/view/Appointment_pages/upcomming.dart';
 
-
 import 'package:graduation_project/view/Messages_Pages/AllMessages.dart';
 import 'package:graduation_project/view/body_model/resultservices.dart';
 import 'package:graduation_project/view/communication_pages/Audio_call_pages/audio_call.dart';
@@ -31,11 +30,6 @@ import 'package:graduation_project/view/registaration_pages/signUp_pages/user_in
 
 import 'view/body_model/click_body.dart';
 
-
-
-
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -45,48 +39,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      routes: {
-        VerificationPage.id: (context) => VerificationPage(),
-        HomePage.id: (context) => HomePage(),
-        NumPad.id: (context) => NumPad(),
-        SignupPage.id: (context) => SignupPage(),
-        LoginPage.id: (context) => LoginPage(),
-        ResetPassword.id: (context) => ResetPassword(),
-        PasswordUpdated.id: (context) => PasswordUpdated(),
-        NoConnectionPage.id: (context) => NoConnectionPage(),
-        AudioCallPage.id: (context) => AudioCallPage(),
-        UserInformation.id: (context) => UserInformation(),
-        BirthDatePage.id: (context) => BirthDatePage(),
-        AudioCallAnswer.id: (context) => AudioCallAnswer(),
-        VideoCallEnd.id : (context) => VideoCallEnd(),
-        VideoCallStart.id : (context) => VideoCallStart(),
-        VideoCallPage2.id : (context) => VideoCallPage2(),
-        VideoCallNoImage.id : (context) => VideoCallNoImage(),
-        suggestionServices.id : (context) => suggestionServices(),
-        Messages.id : (context) => Messages(),
-        AllowLocation.id : (context) => AllowLocation(),
-        ChooseBlood.id : (context) => ChooseBlood(),
-        // ChooseGender.id : (context) => ChooseGender(),
-        // ConsultaionEnd.id : (context) => ConsultaionEnd(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Poppins'),
+        initialRoute: ClickBody.id,
+        routes: {
+          VerificationPage.id: (context) => VerificationPage(),
+          HomePage.id: (context) => HomePage(),
+          NumPad.id: (context) => NumPad(),
+          SignupPage.id: (context) => SignupPage(),
+          LoginPage.id: (context) => LoginPage(),
+          ResetPassword.id: (context) => ResetPassword(),
+          PasswordUpdated.id: (context) => PasswordUpdated(),
+          NoConnectionPage.id: (context) => NoConnectionPage(),
+          AudioCallPage.id: (context) => AudioCallPage(),
+          UserInformation.id: (context) => UserInformation(),
+          BirthDatePage.id: (context) => BirthDatePage(),
+          AudioCallAnswer.id: (context) => AudioCallAnswer(),
+          VideoCallEnd.id: (context) => VideoCallEnd(),
+          VideoCallStart.id: (context) => VideoCallStart(),
+          VideoCallPage2.id: (context) => VideoCallPage2(),
+          VideoCallNoImage.id: (context) => VideoCallNoImage(),
+          suggestionServices.id: (context) => suggestionServices(),
+          Messages.id: (context) => Messages(),
+          AllowLocation.id: (context) => AllowLocation(),
+          ChooseBlood.id: (context) => ChooseBlood(),
+          // ChooseGender.id : (context) => ChooseGender(),
+          // ConsultaionEnd.id : (context) => ConsultaionEnd(),
 
-        ClickBody.id:(context) =>  ClickBody(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      initialRoute: ClickBody.id
+          ClickBody.id: (context) => ClickBody(),
 
-        PesronalInformation.id : (context) => PesronalInformation(),
-        ChooseAppointment.id : (context) => ChooseAppointment(),
-        AppointmentDate.id : (context) => AppointmentDate(),
-        NoDatesAppointment.id : (context) => NoDatesAppointment(),
-        // ChooseGender.id : (context) => ChooseGender(),
-        // ConsultaionEnd.id : (context) => ConsultaionEnd(),
-        
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      initialRoute: ChooseAppointment.id,
-
-    );
+          PesronalInformation.id: (context) => PesronalInformation(),
+          ChooseAppointment.id: (context) => ChooseAppointment(),
+          AppointmentDate.id: (context) => AppointmentDate(),
+          NoDatesAppointment.id: (context) => NoDatesAppointment(),
+          // ChooseGender.id : (context) => ChooseGender(),
+          // ConsultaionEnd.id : (context) => ConsultaionEnd(),
+        });
   }
 }
