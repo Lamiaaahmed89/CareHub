4f3404ae9b371481693aca923144ba49691b846f
Nadeen Noshy <nadeennoshy3@gmail.com>
2023-04-19 00:32:50 +0200

    add page id

--- graduation_project/lib/view/Specialists_Pages/Doctors.dart
+++ graduation_project/lib/view/Specialists_Pages/Doctors.dart
@@ -1,4 +1,3 @@
-
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/reusable/Appbar.dart';
@@ -11,6 +10,7 @@ import 'package:iconsax/iconsax.dart';
 
 class Doctors extends StatelessWidget {
   const Doctors({super.key});
+  static String id = 'Doctors';
 
   @override
   Widget build(BuildContext context) {
@@ -127,7 +127,7 @@ Widget DoctorCard(widtth, heightt) {
                 height: 90,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                      fit: BoxFit.cover, image: AssetImage("assets/abdo.jpg")),
+                      fit: BoxFit.cover, image: AssetImage("assets/images/abdo.jpg")),
                   borderRadius: BorderRadius.all(Radius.circular(8.0)),
                   color: HexColor("#f0f0f0"),
                 ),
